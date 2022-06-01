d6424d072b85bca6108d9d08baece23e39071c5d
coolbeatz71 <sigmacool@gmail.com>
2022-06-01 16:54:18 +0200

    chore: update the typescript path aliases

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,17 @@
-const CracoLessPlugin = require('craco-less');
-const lessToJS = require('less-vars-to-js');
 const fs = require('fs');
 const path = require('path');
+const lessToJS = require('less-vars-to-js');
+const CracoLessPlugin = require('craco-less');
+const { CracoAliasPlugin, configPaths } = require('react-app-rewire-alias');
 
 const modifyVars = lessToJS(fs.readFileSync(path.resolve(__dirname, './src/theme/variables.less'), 'utf8'));
 
 module.exports = {
     plugins: [
+        {
+            plugin: CracoAliasPlugin,
+            options: { alias: configPaths('./tsconfig.paths.json') },
+        },
         {
             plugin: CracoLessPlugin,
             options: {
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import './styles/coreui.min.css';
-import './styles/global.scss';
-import './styles/nprogress.scss';
+import '@styles/coreui.min.css';
+import '@styles/global.scss';
+import '@styles/nprogress.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
