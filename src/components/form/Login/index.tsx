dab04992d64b187f23870b4462e2c8a6880fa1fc
coolbeatz71 <sigmacool@gmail.com>
2022-06-02 13:43:53 +0200

    bug: fix typescript alias and antd not loading issue

--- src/components/form/Login/index.tsx
+++ src/components/form/Login/index.tsx
@@ -1,7 +1,36 @@
 import React, { FC } from 'react';
+import { Button, Form, Input } from 'antd';
+import FloatTextInput from '@components/common/FloatTextInput';
+
+import styles from './index.module.scss';
+
+const { Item } = Form;
+const { Password } = Input;
+
+const btnStyles = `d-flex align-items-center justify-content-center`;
 
 const LoginForm: FC = () => {
-    return <div>lol</div>;
+    return (
+        <div>
+            <Form size="large" name="admin_login" className={styles.loginForm} layout="vertical">
+                <Item name="credential" validateTrigger={['onSubmit', 'onBlur']}>
+                    <FloatTextInput label="Email" placeholder="Email address" required>
+                        <Input size="large" />
+                    </FloatTextInput>
+                </Item>
+
+                <Item name="password" validateTrigger={['onSubmit', 'onBlur']}>
+                    <FloatTextInput label="Password" placeholder="Password" required>
+                        <Password size="large" visibilityToggle />
+                    </FloatTextInput>
+                </Item>
+
+                <Button block size="large" type="primary" htmlType="submit" className={`mt-2 ${btnStyles}`}>
+                    Login
+                </Button>
+            </Form>
+        </div>
+    );
 };
 
 export default LoginForm;
